1ede7dac3fd1df5eec8569c3838be8959e1b3ed1
Ajay Yadav <ajajaj2807@gmail.com>
2020-09-21 12:57:00 +0530

    Add solution for problem `F - Consecutive Subsequence` Link: https://codeforces.com/contest/977/problem/F Tags: dp, *1700 Ref: https://codeforces.com/contest/977/submission/93384874

---
+++ codeforces/977/F.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define pii pair<int, int>
+#define vi vector<int>
+#define si set<int>
+#define mset(arr, val) memset(arr, val, sizeof(arr))
+#define f(i, n) for(int (i)=0;(i)<(n);(i)++)
+
+const int MOD = 1e9+7;
+const int mxN = 1e5;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n];
+    f(i, n) cin >> arr[i];
+    map<int, int> dp;
+    int ans = 0;
+    int mxe = 0;
+    f(i, n) {
+        dp[arr[i]] = max(dp[arr[i]], dp[arr[i]-1]+1);
+        if(dp[arr[i]]>ans) {
+            ans = dp[arr[i]];
+            mxe = arr[i];
+        }
+    }
+    cout << ans << "\n";
+    int prev = 0;
+    for(int i=ans-1;i>=0;--i){
+        auto it =find(arr+prev, arr+n, mxe-i);
+        prev = distance(arr, it);
+        cout << prev+1 << " ";
+    }
+
+}

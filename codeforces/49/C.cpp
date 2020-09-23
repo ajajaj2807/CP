521570e217c9b63b81adfd853ef2023911b8377a
Ajay Yadav <ajajaj2807@gmail.com>
2020-09-23 17:50:00 +0530

    Add solution for problem `C - Disposition` Link: https://codeforces.com/contest/49/problem/C Tags: constructive algorithms, math, *1700 Ref: https://codeforces.com/contest/49/submission/93559063

---
+++ codeforces/49/C.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define pii pair<int, int>
+#define vi vector<int>
+#define si set<int>
+#define mset(arr, val) memset(arr, val, sizeof(arr))
+#define f(i, n) for(int (i)=0;(i)<(n);(i)++)
+#define dbg(x) cout<<#x<<" - "<<x<<"\n";
+
+const int MOD = 1e9+7;
+const int mxN = 1e5;
+
+int main() {
+    int tt;
+    cin >> tt;
+    for(int i=2;i<=tt;++i) cout << i << " ";
+    cout << 1;
+}
